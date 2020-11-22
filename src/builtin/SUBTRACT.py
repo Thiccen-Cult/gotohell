491ce2628da612e28c6855787016dddd0051a97b
Polarzz <57324714+Polarzz@users.noreply.github.com>
2020-11-22 15:44:36 +0000

    Update SUBTRACT.py

--- src/builtin/SUBTRACT.py
+++ src/builtin/SUBTRACT.py
@@ -2,5 +2,5 @@ def main(arg, cells, position):
     try:
         cells[position] = int(arg[0]) - int(arg[1])
     except Exception as e:
-        print("Error: ADD [NUMBER 1] [NUMBER 2]")
+        print("Error: SUBTRACT [NUMBER 1] [NUMBER 2]")
     return {"cells": cells, "position": position}
